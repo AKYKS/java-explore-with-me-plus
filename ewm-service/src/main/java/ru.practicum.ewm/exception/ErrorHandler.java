9499459b85f98106d1c4af68994fcad7cd0ae5d7
escele32 <escele32@yandex.ru>
2026-08-23 08:43:05 +0300

    исправление Checkstyle в ErrorHandler

--- ewm-service/src/main/java/ru.practicum.ewm/exception/ErrorHandler.java
+++ ewm-service/src/main/java/ru.practicum.ewm/exception/ErrorHandler.java
@@ -15,8 +15,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static java.lang.String.format;
-
 @RestControllerAdvice
 public class ErrorHandler {
 
