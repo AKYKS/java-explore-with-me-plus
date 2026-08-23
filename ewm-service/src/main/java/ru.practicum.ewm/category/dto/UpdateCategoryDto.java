138ffcb38bb3bec77e2a46c0003f308dda40dce4
escele32 <escele32@yandex.ru>
2026-08-23 20:58:17 +0300

    исправлена ошибка Checkstyle в UpdateCategoryDto и в EventUserController

--- ewm-service/src/main/java/ru.practicum.ewm/category/dto/UpdateCategoryDto.java
+++ ewm-service/src/main/java/ru.practicum.ewm/category/dto/UpdateCategoryDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.ewm.category.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
--- ewm-service/src/main/java/ru.practicum.ewm/event/controllers/EventUserController.java
+++ ewm-service/src/main/java/ru.practicum.ewm/event/controllers/EventUserController.java
@@ -3,7 +3,6 @@ package ru.practicum.ewm.event.controllers;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.event.dto.EventFullDto;
